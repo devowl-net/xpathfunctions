8af72c0d5aeecad66deebd71426dd6c9946cff1f
SergeyVaulin <SergeyVaulin@localhost>
2018-03-22 00:20:33 +0300

    second commit

---
+++ XPathExamples/Common/ExampleBase.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Xml.XPath;
+
+namespace XPathExamples.Common
+{
+    /// <summary>
+    /// Example base class.
+    /// </summary>
+    public abstract class ExampleBase
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Execute example.
+        /// </summary>
+        public abstract void Execute();
+
+        /// <summary>
+        /// Create <see cref="XPathDocument"/> from <see cref="xmlSource"/> string.
+        /// </summary>
+        /// <param name="xmlSource">Source xml string.</param>
+        /// <returns><see cref="XPathDocument"/> instance.</returns>
+        protected XPathDocument XPathDocumentFromString(string xmlSource)
+        {
+            using (var stringReader = new StringReader(xmlSource))
+            {
+                return new XPathDocument(stringReader);
+            }
+        }
+    }
+}
---
+++ XPathExamples/Examples/IfElseExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+using XPathExamples.Common;
+
+namespace XPathExamples.Examples
+{
+    /// <summary>
+    /// if-else function example.
+    /// </summary>
+    public class IfElseExample : ExampleBase
+    {
+        /// <inheritdoc/>
+        public override string Name => @"""if-else"" conditional function";
+
+        /// <inheritdoc/>
+        public override void Execute()
+        {
+            var sourceXml = @"
+                <?xml version=""1.0""?>
+                <catalog>
+                    <book id=""bk101"">
+                        <author gender=""male"">Gambardella, Matthew</author>
+                        <title>XML Developer's Guide</title>
+                        <genre>Computer</genre>
+                        <price>44.95</price>
+                        <publish_date>2000-10-01</publish_date>
+                        <description>An in-depth look at creating applications 
+                        with XML.</description>
+                    </book>
+                </catalog>";
+
+            var document = XPathDocumentFromString(sourceXml);
+            var navigator = document.CreateNavigator();
+            var query = @"
+                if-else
+                (
+                    /catalog/book/autor/@gender = 'male' and /catalog/book/genre/text() = 'Computer',
+                    '[true]  Autor is male',
+                    '[false] Autor is female'
+                )";
+
+            navigator.Select(query, CreateManager());
+        }
+
+        private IXmlNamespaceResolver CreateManager()
+        {
+            
+        }
+    }
+}
